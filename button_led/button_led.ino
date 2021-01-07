bab6ab7f0b5deb586d6fd130ebde5b6987cd38c1
blake27182 <duckbill43@gmail.com>
2021-01-07 17:27:50 -0500

    first commit

---
+++ button_led/button_led.ino
@@ -0,0 +1,30 @@
+#define ron 6
+#define led 12
+
+bool state = false;
+bool button;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ron, INPUT);
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  button = digitalRead(ron);
+  if (button){
+    if (!state){
+      digitalWrite(led, HIGH);
+      state = true;
+    } else {
+      digitalWrite(led, LOW);
+      state = false;
+    }
+    while(button){
+      button = digitalRead(ron);
+      delay(10);
+    }
+  }
+    
+  delay(10);
+}
